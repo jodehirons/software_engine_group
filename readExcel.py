185ab4bf42568a6a9eb13d3dcf6b5cc1ea992509
hiuboom <hiuboom2726@gmail.com>
2023-11-04 14:51:43 +0800

    读取excel

---
+++ readExcel.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+# 从Excel文件读取数据，跳过第一行
+# 自己输入文件路径
+# 示例,再优化
+a = "成绩表.xlsx"
+
+# 将数据转化为包含字典的列表
+class Student():
+    
+    def __init__(self,a) :
+        self.a = a
+        self.student_list = []
+        self.readExcel()
+
+    def readExcel(self):
+
+        # 读取Excel文件
+        data = pd.read_excel(self.a, skiprows=1)
+
+        for index, row in data.iterrows():
+            student_info = {
+                '学号': row['学号'],
+                '姓名': row['姓名'],
+                "选课时间" : row['选课时间'],
+                "课程名称" : row['课程名称'].replace('\xa0', ' '),
+                "学分" : row['学分'],
+                "百分成绩":row['百分成绩'],
+                "五分成绩":row['五分成绩'],
+                "考试类型":row['考试类型'],
+                "选修类型":row['选修类型'].replace('\xa0', ' '),
+                # 添加其他学生信息字段
+            }
+            self.student_list.append(student_info)
+
+    # 打印包含学生信息的列表 student_list
+    def showAll(self):
+        for student in self.student_list:
+            print(student)
+
+if __name__ == '__main__':
+    student = Student(a)
+    student.showAll()
